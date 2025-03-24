fb99851c71662d713823518bab8ebccd03e99ddf
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-03-24 10:48:05 +0100

    add enabled query to not send http request at first loading

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -7,11 +7,15 @@ import EventItem from './EventItem';
 
 export default function FindEventSection() {
   const searchElement = useRef();
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState();
 
-  const {data, isPending, isError, error} = useQuery({
+  // differenza tra isPending e isLoading, 
+  // è che il primo anche se disabilitata è vero in quanto rimane in attesa dei dati come se non fossero disponibili lato server
+  // mentre il secondo è attivo solo quando realmente viene inviata la richiesta.
+  const {data, isLoading, isError, error} = useQuery({
     queryKey:['events', {search: searchTerm}],
-    queryFn: ({signal}) => fetchEvents({signal, searchTerm})
+    queryFn: ({signal}) => fetchEvents({signal, searchTerm}),
+    enabled: searchTerm !== undefined
   })
 
   function handleSubmit(event) {
@@ -21,7 +25,7 @@ export default function FindEventSection() {
 
   let content= <p>Please enter a search term and to find events.</p>;
 
-  if(isPending){
+  if(isLoading){
     content= <LoadingIndicator />
   }
 
@@ -29,7 +33,7 @@ export default function FindEventSection() {
     content = <ErrorBlock title='An errore occurred' message={error.info?.message || 'Failed to fetche event.'}/>
   }
 
-  if(data?.length > 0){
+  if(data){
     content = (
       <ul className="events-list">
         {data.map((event) => (
@@ -39,8 +43,6 @@ export default function FindEventSection() {
         ))}
       </ul>
     );
-  }else{
-    content = <p>No event to your search</p>
   }
 
   return (
